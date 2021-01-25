de582c183a761abfbc78aeb0639067fd6dd9a180
Akila Mendis <akila@oneit.com.au>
2021-01-25 09:15:57 +0530

    initial commit

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string con = "Server=127.0.0.1;Uid=root;Pwd=123;Database=san;";
+        public Form1()
+        {
+
+            con = "Server=127.0.0.1;Uid=root;Pwd=123;Database=san;";
+            InitializeComponent();
+        }
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                sanEntities db = new sanEntities();
+                master ms = new master();
+                ms.idmaster = Convert.ToInt16(textBox1.Text);
+                ms.name = textBox2.Text;
+                db.AddObject("master", ms);
+               int val=db.SaveChanges();
+               if (val == 0)
+               {
+                   MessageBox.Show("Unsuccessfull");
+               }
+               else 
+               {
+                   MessageBox.Show("Successfull");
+               }
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            // Open App.Config of executable
+            //System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);    
+            // Add an Application Setting.
+            //config.AppSettings.Settings.Remove("connectionString");
+            //config.AppSettings.Settings.Add("connectionString", "provider=MySql.Data.MySqlClient;provider connection string=&quot;server=localhost;User Id=root;database=san;password=123&quot;");
+        // Save the configuration file.
+            //config.Save(ConfigurationSaveMode.Modified);
+        // Force a reload of a changed section.
+            //ConfigurationManager.RefreshSection("appSettings");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
